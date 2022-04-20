b743bb0d15c5526695e58cfd885445a3fa779ba8
unparalleled-js <yingthi@live.com>
2022-04-20 14:19:02 +0200

    feat: get nonce

--- tests/functional/test_provider.py
+++ tests/functional/test_provider.py
@@ -1,3 +1,13 @@
+def test_get_nonce(provider, account, contract):
+    initial_nonce = provider.get_nonce(account.contract_address)  # type: ignore
+
+    # Transact to increase nonce
+    contract.increase_balance(account.address, 123, sender=account)
+
+    actual = provider.get_nonce(account.contract_address)  # type: ignore
+    assert actual == initial_nonce + 1
+
+
 def test_contract_at(contract, provider):
-    actual = provider.contract_at(contract.address)
+    actual = provider.contract_at(contract.address)  # type: ignore
     assert actual.address == contract.address
