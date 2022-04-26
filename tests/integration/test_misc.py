345fbcfa9524fa2821579e6883817e41c3b2fa8f
unparalleled-js <yingthi@live.com>
2022-04-26 13:41:17 +0200

    chore: try pinnig to lower versions

--- tests/integration/test_misc.py
+++ tests/integration/test_misc.py
@@ -3,5 +3,5 @@ from .conftest import ApeStarknetCliRunner
 
 def test_plugin_loads(ape_cli):
     runner = ApeStarknetCliRunner(ape_cli, ["starknet"])
-    output = runner.invoke("--help", "-v", "debug")
+    output = runner.invoke("--help")
     assert "ERROR" not in runner.invoke("--help"), output
