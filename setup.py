01a0d2213914d740515cc59cc5e58350fe5d5b75
unparalleled-js <yingthi@live.com>
2022-06-03 08:47:50 -0500

    chore: more dep updates

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ extras_require = {
     "lint": [
         "black>=22.3.0,<23.0",  # auto-formatter and linter
         "mypy>=0.950,<1.0",  # Static type analyzer
-        "flake8>=3.9.2,<4.0",  # Style linter
+        "flake8>=4.0.1,<5.0",  # Style linter
         "isort>=5.10.1,<6.0",  # Import sorting linter
         "types-pkg-resources>=0.1.3,<0.2",
     ],
@@ -69,7 +69,7 @@ setup(
         "ethpm-types",  # Use same as `eth-ape`.
         "pydantic>=1.9.0,<2.0",
         "starknet.py>=0.2.3a0,<0.2.4",
-        "starknet-devnet>=0.2.1",
+        "starknet-devnet>=0.2.2",
         "importlib-metadata ; python_version<'3.8'",
     ],  # NOTE: Add 3rd party libraries here
     entry_points={"ape_cli_subcommands": ["ape_starknet=ape_starknet._cli:cli"]},
