25298aaaa8cfc958e22cf5244453a71d53bdc0e6
0xca11ab1e <105989135+ca11ab1e@users.noreply.github.com>
2022-07-01 14:48:10 +0200

    fix event log arguments handling

--- tests/functional/test_tokens.py
+++ tests/functional/test_tokens.py
@@ -44,3 +44,24 @@ def test_transfer(tokens, account, second_account, token):
     initial_balance = tokens.get_balance(second_account.address, token=token)
     tokens.transfer(account.address, second_account.address, 10, token=token)
     assert tokens.get_balance(second_account.address, token=token) == initial_balance + 10
+
+
+def test_event_log_arguments(token_contract, account, second_account):
+    amount = 42 * 10**42
+    amount_uint256 = (308580416249208569525703126576732831744, 123426)
+    receipt = token_contract.transfer(second_account.address, amount, sender=account)
+
+    transfer_logs = list(receipt.decode_logs(token_contract.Transfer))
+    assert len(transfer_logs) == 1
+    log = transfer_logs[0]
+    assert log.from_ == int(account.address, 16)
+    assert log.to == int(second_account.address, 16)
+    assert log.value == amount_uint256
+
+    mint_logs = list(receipt.decode_logs(token_contract.Mint))
+    assert len(mint_logs) == 1
+    log = mint_logs[0]
+    assert log.sender == int(account.address, 16)
+    assert log.amount0 == amount_uint256
+    assert log.amount1 == (10, 0)
+    assert log.to == int(second_account.address, 16)
