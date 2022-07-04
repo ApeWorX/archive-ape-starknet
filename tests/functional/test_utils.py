86b63f578b1bd634a01e3c489ba49463676d2162
unparalleled-js <yingthi@live.com>
2022-07-04 10:39:52 -0500

    test: correct test

--- tests/functional/test_utils.py
+++ tests/functional/test_utils.py
@@ -10,4 +10,4 @@ def test_get_random_private_key(iteration):
     assert len(pkey) == 66
     pkey_int = int(pkey, 16)
     pkey_back_to_str = HexBytes(pkey_int).hex()
-    assert pkey_back_to_str == pkey
+    assert pkey_back_to_str.replace("0x", "") in pkey
