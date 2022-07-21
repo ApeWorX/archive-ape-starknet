2002f3bb75ca91cc467ac46a53ac782722355497
0xca11ab1e <105989135+ca11ab1e@users.noreply.github.com>
2022-07-21 10:26:38 +0200

    fix: mypy + improve doc

--- ape_starknet/provider.py
+++ ape_starknet/provider.py
@@ -269,14 +269,14 @@ class StarknetProvider(SubprocessProvider, ProviderAPI, StarknetBase):
             abi = txn.method_abi
             full_abi = txn.contract_type.abi
 
-            if txn.original_method_abi:
-                # When that special attribute is set means the transation came from an
-                # account-specific call: it implies the original method ABI was replaced
-                # with the specific execute ABI in BaseStarknetAccount.prepare_transaction(),
+            if txn.original_method_abi and txn.original_full_abi:
+                # When those special attributes are set means the transation came from an
+                # account-specific call: it implies original method ABIs were replaced
+                # with account-specific ABIs in BaseStarknetAccount.prepare_transaction(),
                 # and that the return data is always prefixed with the number of items.
                 # We need to restore the former, and remove the later.
                 abi = txn.original_method_abi
-                full_abi = txn.original_full_abi  # type: ignore
+                full_abi = txn.original_full_abi
                 returndata = returndata[1:]
 
             return_value = self.starknet.decode_returndata(abi, returndata, full_abi)
