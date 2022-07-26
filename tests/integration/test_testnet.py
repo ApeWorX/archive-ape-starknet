2db286c8559072c8468b80d8200ed39944a0bd97
0xca11ab1e <105989135+ca11ab1e@users.noreply.github.com>
2022-07-26 11:25:51 +0200

    refactor: simplify, and pythonify, TokenManager.is_token()

--- tests/integration/test_testnet.py
+++ tests/integration/test_testnet.py
@@ -4,3 +4,11 @@ def test_can_connect_to_testnet(networks):
 
         # Make call
         eth_contract.balanceOf("0x348ef2b95e31269b4a1c019428723e3a33cd964f92ad866741f189b88be3bc0")
+
+
+def test_is_token(networks, tokens):
+    with networks.parse_network_choice("starknet:testnet") as provider:
+        eth_contract = provider.tokens["eth"]
+
+        # Ensure it's recognized as a token
+        assert tokens.is_token(eth_contract)
