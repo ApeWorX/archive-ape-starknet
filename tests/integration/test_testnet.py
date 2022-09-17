6e82a3d826e2427a3cc4df250c5540cf24db71b5
unparalleled-js <yingthi@live.com>
2022-09-16 19:44:35 -0500

    test: fixes

--- tests/integration/test_testnet.py
+++ tests/integration/test_testnet.py
@@ -13,9 +13,5 @@ def eth_contract(testnet_provider):
 
 
 def test_can_connect_to_testnet(eth_contract):
-    eth_contract.balanceOf("0x348ef2b95e31269b4a1c019428723e3a33cd964f92ad866741f189b88be3bc0")
-
-
-def test_is_token(eth_contract, tokens):
-    # Ensure it's recognized as a token
-    assert tokens.is_token(eth_contract)
+    address = "0x348ef2b95e31269b4a1c019428723e3a33cd964f92ad866741f189b88be3bc0"
+    assert eth_contract.balanceOf(address) is not None
